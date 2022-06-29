00e7f652ebe34fa5ed7613569771d356bfbf1ef0
Marcio Sfalsin <marciosfalsin@gmail.com>
2022-06-29 18:41:41 -0300

    http

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() => runApp(MyApp());
