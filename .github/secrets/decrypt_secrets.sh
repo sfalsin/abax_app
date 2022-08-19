4f3f9713bdd0b240ef04e70f8d658035577d7697
Marcio Sfalsin <marciosfalsin@gmail.com>
2022-08-19 09:46:32 -0300

    senhas

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -9,11 +9,11 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/YOUR_PROFILE.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/YOUR_PROFILE.mobileprovision
 
 
-security create-keychain -p "" build.keychain
-security import ./.github/secrets/YOUR_CERTIFICATE.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security create-keychain -p "hezq-ezxm-rdln-whyg" build.keychain
+security import ./.github/secrets/YOUR_CERTIFICATE.p12 -t agg -k ~/Library/Keychains/build.keychain -P "hezq-ezxm-rdln-whyg" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security unlock-keychain -p "hezq-ezxm-rdln-whyg" ~/Library/Keychains/build.keychain
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "hezq-ezxm-rdln-whyg" ~/Library/Keychains/build.keychain
