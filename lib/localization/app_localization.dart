b7f1bc86b8524852e5f1d8d89c3d024104bc577e
Marcio Sfalsin <marciosfalsin@gmail.com>
2023-01-27 14:17:46 -0300

    try2

--- lib/localization/app_localization.dart
+++ lib/localization/app_localization.dart
@@ -1,7 +1,8 @@
+import 'package:abax/localization/pt_br/pt_br_translations.dart';
 import 'package:get/get.dart';
 import 'en_us/en_us_translations.dart';
 
 class AppLocalization extends Translations {
   @override
-  Map<String, Map<String, String>> get keys => {'en_US': enUs};
+  Map<String, Map<String, String>> get keys => {'pt_BR': ptBr};
 }
